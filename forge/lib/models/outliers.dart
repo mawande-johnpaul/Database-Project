591f996595a03fdf787c3ecc7944cf206bcdd318
prechardmichelle <prechardmichelle@gmail.com>
2025-10-07 08:55:28 -0700

    edits

--- forge/lib/models/outliers.dart
+++ forge/lib/models/outliers.dart
@@ -5,7 +5,7 @@ List<double> removeOutliers(List<double> data) {
   double q3 = data[(3 * n / 4).floor()];
   double iqr = q3 - q1;
   double lowerBound = q1 - 1.5 * iqr;
-  double bigBound = q3 + 1.5 * iqr;
+  double upperBound = q3 + 1.5 * iqr;
 
 
   return data.where((x) => x >= lowersBound && x <= upperBound).toList();
